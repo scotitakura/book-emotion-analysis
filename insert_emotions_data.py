6c134010bfe94f03bf58f235cbf9ffa7fe59169c
scotitakura <scot.itakura@gmail.com>
2022-05-15 18:19:37 -0700

    Updated and consolidated gathering and intputing data to db

--- insert_emotions_data.py
+++ insert_emotions_data.py
@@ -1,77 +1,108 @@
 import sys
 import sqlite3
+import logging
 from sqlite3 import Error
+from time import perf_counter
 import text2emotion as te
+from prefect import flow, task
 
-def main(book_name):
-    database = "C:\sqlite\db\emotions.db"
+database = "/mnt/c/sqlite/db/emotions.db"
+file_logger = logging.getLogger(__name__)
+file_logger.setLevel(logging.INFO)
 
-    def create_connection(db_file):
-        """
-        Function connects to a SQLite database.
-        
-        Parameters
-        ----------
-        db_file : String
-            File path of the database you would like to connect to.
-            
-        Return
-        ------
-        Connection obj or None.
-        """
-        
-        conn = None
+@task
+def create_table(book_name):
+    """
+    Connects to database and then creates a table if one doesn't exist for a specific book.
+    Parameters
+    ----------
+    book_name : String
+        String of a book name with a text file of that book.
+    Returns
+    -------
+    None
+    """
+    sql_create_table = f""" CREATE TABLE IF NOT EXISTS {book_name}_table (
+                                id integer PRIMARY KEY,
+                                paragraph VARCHAR(3000),
+                                paragraph_length int,
+                                happy float,
+                                angry float,
+                                surprise float,
+                                sad float,
+                                fear float,
+                                dominant_emotion VARCHAR(50),
+                                log_runtime float
+                                ); """
+
+    conn = None
+    try:
+        conn = sqlite3.connect(database)
+    except Error as e:
+        print(e)
+    
+    if conn is not None:
         try:
-            conn = sqlite3.connect(db_file)
+            c = conn.cursor()
+            c.execute(sql_create_table)
         except Error as e:
             print(e)
-            
-        return conn
+    else:
+        print("Error! Cannot create the database connection!")
 
-    def insert_data(conn, project):
-        """
-        Insert new values into the weather_table.
-        
-        Parameters
-        ----------
-        conn : conn
-        project : string, string, datetime, float
-            weather_table values as city_name, state_name, datetime, and temperature
-            
-        Returns
-        -------
-        None
-        """
-        
-        sql = f'''
-            INSERT INTO {book_name}_table (paragraph, paragraph_length, happy, angry, surprise, sad, fear, dominant_emotion)
-            VALUES(?,?,?,?,?,?,?,?)
-            '''
-        cur = conn.cursor()
-        cur.execute(sql, project)
-        rowid = cur.lastrowid
-        conn.commit()
-        return rowid
-
-    def listToString(s):
-        str1 = ", "
-        return(str1.join(s))
-
-    conn = create_connection(database)
-
-    with open(f'./text_files/{book_name}.txt', 'r', encoding="utf8") as f:
-        gatsby = f.read()
-
-    tests = gatsby.split("\n\n")
+@task
+def create_data(book_name):
+    """
+    Function that parses a text file into paragraphs.
+    Parameters
+    ----------
+    book_name : String
+        String of a book name with a text file of that book.
+    Return
+    ------
+    A list of paragraphs.
+    """
     paragraphs = []
+    
+    with open(f'./text_files/{book_name}.txt', 'r', encoding="utf8") as f:
+        book_text = f.read()
+
+    tests = book_text.split("\n\n")
 
     for paragraph in tests:
-        if paragraph != "------------------------------------------------------------------------":
-            paragraphs.append(paragraph.replace("\n", " "))
+        paragraphs.append(paragraph.replace("\n", " "))
 
     print("Number of paragraphs in this book: ", len(paragraphs))
 
+    return paragraphs
+
+@task
+def insert_data(paragraphs, book_name):
+    """
+    Insert new values into the weather_table.
+    Parameters
+    ----------
+    conn : conn
+    project : string, string, datetime, float
+        weather_table values as city_name, state_name, datetime, and temperature
+    Returns
+    -------
+    None
+    """
+    sql = f'''
+        INSERT INTO {book_name}_table (paragraph, paragraph_length, happy, angry, surprise, sad, fear, dominant_emotion, log_runtime)
+        VALUES(?,?,?,?,?,?,?,?,?)
+        '''
+
+    conn = None
+    try:
+        conn = sqlite3.connect(database)
+    except Error as e:
+        print(e)
+
     for paragraph in paragraphs:
+        start_time = perf_counter()
+
         paragraphs_to_emotions = te.get_emotion(paragraph)
         dominant_emotion = ""
 
@@ -86,9 +117,37 @@ def main(book_name):
                         paragraphs_to_emotions["Surprise"],
                         paragraphs_to_emotions["Sad"],
                         paragraphs_to_emotions["Fear"],
-                        listToString(dominant_emotion))
+                        ", ".join(dominant_emotion),
+                        0)
 
-        insert_data(conn, emotion_results)
+        cur = conn.cursor()
+        cur.execute(sql, emotion_results)
+        row_id = cur.lastrowid
+        conn.commit()
+
+        end_time = perf_counter()
+        total_time = end_time - start_time
+        try:
+            file_logger.info('Paragraph Id: %i, Paragraph Length: %i, Execution Time: %f', row_id, len(paragraph), total_time)
+        except Error as e:
+            file_logger.error(row_id, e)
+
+        update_sql = f'''
+          UPDATE {book_name}_table
+          SET log_runtime = ?
+          WHERE id = ?
+          '''
+
+        cur.execute(update_sql, (total_time, row_id))
+
+@flow
+def main(book_name):
+    file_handler = logging.FileHandler(f'logs/{book_name}.log')
+    file_logger.addHandler(file_handler)
+
+    create_table(book_name)
+    paragraph_data = create_data(book_name)
+    insert_data(paragraph_data, book_name)
 
 if __name__ == '__main__':
     main(sys.argv[1])
