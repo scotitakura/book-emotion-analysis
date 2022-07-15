a0d67da80acfe058d4d5dcd9b1b4b7d7f6fdc59d
scotitakura <scot.itakura@gmail.com>
2022-07-14 22:17:32 -0700

    Fixing cloud to mysql

--- insert_emotions_cloud.py
+++ insert_emotions_cloud.py
@@ -31,6 +31,7 @@ def create_table(book_name):
     """
     
     sql_create_table = f""" CREATE TABLE IF NOT EXISTS {book_name}_table (
+                                paragraph_num int,
                                 paragraph VARCHAR(3000),
                                 paragraph_length int,
                                 fear int,
